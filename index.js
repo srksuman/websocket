25c291ee89a25be94b2e0bc91b8c39c2363ec21c
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-04-05 12:31:59 +0545

    Changed env variables

--- index.js
+++ index.js
@@ -3,64 +3,71 @@ require("dotenv").config();
 const { Client, Intents } = require("discord.js");
 const axios = require("axios");
 
-const client_one = new Client({
+const gbetRateUsdClient = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-const client_two = new Client({
+const floorPriceClient = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 const GBET =
   "https://api.coingecko.com/api/v3/simple/price?ids=gangstabet&vs_currencies=usd";
-const FLOOR_PRICE =
-  "https://gangsta-node-main.herokuapp.com/api/marketmetrics/floorprice";
+const FLOOR_PRICE = process.env.GANGSTA_URL+"/api/marketmetrics/floorprice";
 
-client_one.on("ready", async () => {
-  console.log("starting one");
-  let price = "*";
+// gbet
+gbetRateUsdClient.on("ready", async () => {
+  let price ;
+  try{
   axios.get(GBET).then((res) => {
     price = res.data.gangstabet.usd;
   });
-  client_one.user.setActivity("$GBET", { type: "WATCHING" });
-  client_one.guilds.cache.forEach((guild) => {
+  }catch{
+    price = "Fetching"
+  }
+  gbetRateUsdClient.user.setActivity("$GBET", { type: "WATCHING" });
+  gbetRateUsdClient.guilds.cache.forEach((guild) => {
     setInterval(() => {
       try {
         axios.get(GBET).then((res) => {
           price = res.data.gangstabet.usd;
-          console.log("Worked Successfully one");
+          console.log("gbet price fetched");
           console.log(price);
         });
       } catch {
-        console.log("Error");
+        console.log("Error fetching gbet price");
       }
       guild.me.setNickname(`$${price}/GBET`);
     }, 40 * 1000);
   });
 });
 
-client_two.on("ready", async () => {
-  console.log("starting two");
-  let price = "*";
+//floorprice
+floorPriceClient.on("ready", async () => {
+  let price;
+  try{
   axios.get(FLOOR_PRICE).then((res) => {
     price = res.data.floor_price;
-  });
-  client_two.user.setActivity("GangstaBet's floor price", { type: "WATCHING" });
-  client_two.guilds.cache.forEach((guild) => {
+  });}
+  catch{
+    price = "Fetching"
+  }
+  floorPriceClient.user.setActivity("GangstaBet's floor price", { type: "WATCHING" });
+  floorPriceClient.guilds.cache.forEach((guild) => {
     setInterval(() => {
       try {
         axios.get(FLOOR_PRICE).then((res) => {
           price = res.data.floor_price;
-          console.log("Worked Successfully two");
+          console.log("Floor price fetched");
           console.log(price);
         });
       } catch {
         price = price;
-        console.log("Error");
+        console.log("Error fetching Floor price");
       }
       guild.me.setNickname(`FP: ${price}${" "} ICX`);
     }, 40 * 1000);
   });
 });
-client_one.login(process.env.BOT_TOKEN_ONE);
-client_two.login(process.env.BOT_TOKEN_TWO);
+gbetRateUsdClient.login(process.env.BOT_TOKEN_ONE);
+floorPriceClient.login(process.env.BOT_TOKEN_TWO);

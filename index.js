8d6f054b33872c7ac77be7bd40294fda28030569
suman <sumanrajkhanal@gmail.com>
2022-07-16 00:10:07 +0545

    change in try catch

--- index.js
+++ index.js
@@ -199,14 +199,18 @@ floorPriceClientNFT.on("ready", async () => {
 const floorPriceBribe = async () => {
   let priceArray = [];
   for (let i = 1; i <= 8; i++) {
-    let url = `https://api.craft.network/nft?tokenIds=${i}&collectionId=cx69fd9c7587dc8022b1e761d127b35cc354f96b6c&limit=20&orderDirection=desc`;
-    const data = await axios.get(url);
+    try {
+      let url = `https://api.craft.network/nft?tokenIds=${i}&collectionId=cx69fd9c7587dc8022b1e761d127b35cc354f96b6c&limit=20&orderDirection=desc`;
+      const data = await axios.get(url);
 
-    const lowestPrice =
-      data.data.data[0][`cx69fd9c7587dc8022b1e761d127b35cc354f96b6c:${i}`]
-        .lowestPrice;
-    if (lowestPrice !== undefined) {
-      priceArray.push(lowestPrice);
+      const lowestPrice =
+        data.data.data[0][`cx69fd9c7587dc8022b1e761d127b35cc354f96b6c:${i}`]
+          .lowestPrice;
+      if (lowestPrice !== undefined) {
+        priceArray.push(lowestPrice);
+      }
+    } catch {
+      priceArray.push(0);
     }
   }
   const floorValue = Math.min(...priceArray);
